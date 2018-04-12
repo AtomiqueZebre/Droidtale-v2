d97349c6e8ecef092d3b81e6be4cadbb80f12605
AtomiqueZebre <38320442+AtomiqueZebre@users.noreply.github.com>
2018-04-12 12:52:48 +0000

    Delete FileStatus.java

--- src/com/mrpowergamerbr/droidtale/FileStatus.java
+++
@@ -1,9 +0,0 @@
-package com.mrpowergamerbr.droidtale;
-
-public enum FileStatus {
-	VALID, INVALID_CHECKSUM, MISSING_ASSETS, ERROR, NOT_SET;
-	
-	public boolean go() {
-		return (this == VALID || this == INVALID_CHECKSUM);
-	}
-}
